382432919ed0c7f752b0cc9d7a12ef9f588d8e9f
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-02 22:28:51 +0000

    No, it still does that so I'm assuming that's because I haven't actually rendered anything

--- gameInitialisation.py
+++ gameInitialisation.py
@@ -2,5 +2,6 @@ import pygame
 def init():
     pygame.init() #Simply initialises pygame and all its modules
     screen = pygame.display.set_mode([1920,1080],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
-
+    #A few more comments to explain the above:
+    #Essentially, the first part sets the resolution, the second part is a flag that ensures that it is fullscreen, the third enables the correct colours to be used, the fourth enables the default display to be selected, whilst the last one turns off Vsync
 init()
