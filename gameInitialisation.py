a054413e8a71c0b1730d55d53e8c1e5cd123472c
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-03 14:28:06 +0000

    Implemented the successfully tested method from earlier by obtaining the default desktop resolution and then using that as the screen size

--- gameInitialisation.py
+++ gameInitialisation.py
@@ -1,9 +1,9 @@
 import pygame
 def init():
     pygame.init() #Simply initialises pygame and all its modules
-    #screen = pygame.display.set_mode([pygame.display.get_desktop_sizes],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
+    display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all the displays
+    screen = pygame.display.set_mode(display_list[0],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
     #A few more comments to explain the above:
     #Essentially, the first part sets the resolution, the second part is a flag that ensures that it is fullscreen, the third enables the correct colours to be used, the fourth enables the default display to be selected, whilst the last one turns off Vsync
-    display_list = pygame.display.get_desktop_sizes()
-    print(display_list[0])
+
 init()
