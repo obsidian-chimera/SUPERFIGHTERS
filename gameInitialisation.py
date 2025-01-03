298a28d7458e562c50701ec980981b06b50261e5
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-03 15:58:44 +0000

    Created clock object to track time

--- gameInitialisation.py
+++ gameInitialisation.py
@@ -4,6 +4,6 @@ def init():
     display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all the displays
     screen = pygame.display.set_mode(display_list[0],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
     #A few more comments to explain the above:
-    #Essentially, the first part sets the resolution, the second part is a flag that ensures that it is fullscreen, the third enables the correct colours to be used, the fourth enables the default display to be selected, whilst the last one turns off Vsync
-
+    #Essentially, the first part sets the resolution of the default display, the second part is a flag that ensures that it is fullscreen, the third enables the correct colours to be used, the fourth enables the default display to be selected, whilst the last one turns off Vsync
+    clock = pygame.time.Clock #Creates an object called clock to track time
 init()
