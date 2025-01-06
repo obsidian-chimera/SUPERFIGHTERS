33a96116c02b6778df0a299255c6601db45a42bf
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-06 04:10:23 +0000

    Added font initialisation

--- gameInitialisation.py
+++ gameInitialisation.py
@@ -1,6 +1,7 @@
 import pygame as pyg #I realised it would be easier later on to shorten pygame to pyg
 def init():
     pyg.init() #Simply initialises pyg and all its modules
+    pyg.font.init() #Initialise the fonts I want to use
     display_list = pyg.display.get_desktop_sizes() #Obtains resolutions of all the displays
     screen = pyg.display.set_mode(display_list[0],flags=pyg.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
     #A few more comments to explain the above:
