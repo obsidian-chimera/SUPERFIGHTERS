67c9184a14d5aba8d7828e6c857a0b25a703ffd6
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-06 04:21:14 +0000

    I added the game_state variable directly into the code as a global variable because it is one of the few variables that will be needed everywhere and I think it would be a good idea to cast it as a global variable. I also tried adding a game_state_update procedure but I don't know how to implement that

--- gameLoopUpdate.py
+++ gameLoopUpdate.py
@@ -1,5 +1,11 @@
 import pygame as pyg
+
+game_state = bool(False)
+
 def game_loop(game_state):
     while game_state:
         pyg.display.flip() #This updates the display
         pyg.time.Clock().tick(250) #This is used to cap the frame rate to minimise unnecessary system resource usage
+
+def game_state_update():
+    #Unsure as to how to implement this as of yet - more research is needed
