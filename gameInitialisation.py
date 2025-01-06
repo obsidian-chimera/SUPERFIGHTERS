792dfdda125b82e14ccca855c58d05c861c234a1
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-06 04:06:55 +0000

    Realised that shortening pygame to pyg makes it easier to type out

--- gameInitialisation.py
+++ gameInitialisation.py
@@ -1,8 +1,8 @@
-import pygame
+import pygame as pyg #I realised it would be easier later on to shorten pygame to pyg
 def init():
-    pygame.init() #Simply initialises pygame and all its modules
-    display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all the displays
-    screen = pygame.display.set_mode(display_list[0],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
+    pyg.init() #Simply initialises pyg and all its modules
+    display_list = pyg.display.get_desktop_sizes() #Obtains resolutions of all the displays
+    screen = pyg.display.set_mode(display_list[0],flags=pyg.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
     #A few more comments to explain the above:
     #Essentially, the first part sets the resolution of the default display, the second part is a flag that ensures that it is fullscreen, the third enables the correct colours to be used, the fourth enables the default display to be selected, whilst the last one turns off Vsync
-    clock = pygame.time.Clock #Creates an object called clock to track time
+    clock = pyg.time.Clock #Creates an object called clock to track time
