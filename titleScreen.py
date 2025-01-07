8a3c24efe72e0296512b8b4385c68fbbe9778cde
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-07 07:04:54 +0000

    Started on loop - trialling everything in titleScreen file for now

--- titleScreen.py
+++ titleScreen.py
@@ -9,4 +9,5 @@ start = Button("start","START GAME",(100,100),(200,200), (0,0,0))
 quit = Button("quit","QUIT GAME",(100,150),(200,200), (0,0,0))
 
 game_state = bool(True)
-while game_state:
+# while game_state:
+#     pass
