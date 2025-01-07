07fd29ade675862d8fb5d8601a5dd5be106e9986
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-07 06:51:17 +0000

    I created part of a button class but more research is needed to figure out how to get it to do something when clicked

--- classes..py
+++ classes..py
@@ -7,5 +7,15 @@ class Button:
         self.colour = colour
         self.font = pygame.font.SysFont("Arial",40,True)
         self.rect = pygame.Rect(self.position,self.size)
-        self.text_surface = self.font.render(self.text,True,(0,0,0))
-        
+        self.surface = self.font.render(self.text,True,(0,0,0))
+
+        # Constructor method to setup attributes for the button
+
+    def draw(self, screen):
+        pygame.draw.rect(screen,self.colour,self.rect)
+        screen.blit(self.text_surface,self.rect)
+
+        # Method to draw the button onto the screen with the text superimposed on top of the button background
+
+    
+
