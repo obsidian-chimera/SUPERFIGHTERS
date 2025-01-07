b79862f9e00ef4d94bd02a11f52027eedfdf3513
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-07 06:39:13 +0000

    Returned pygame to pygame from pyg

---
+++ classes..py
@@ -0,0 +1,10 @@
+import pygame
+class Button:
+    def __init__(self, text, position, size, colour):
+        self.text = text
+        self.position = position
+        self.size = size
+        self.colour = colour
+        self.font = pygame.font.SysFont("Arial",40,True)
+        self.rect = pygame.Rect(self.position,self.size)
+        self.text_surface = self.font.render(self.text,True,(0,0,0))
--- init.py
+++ init.py
@@ -1,19 +1,19 @@
-import pygame as pyg #I realised it would be easier later on to shorten pygame to pyg
-import pygame_menu as pyg_m #Verily easily creates menu screens
+import pygame 
+import pygame_menu #Verily easily creates menu screens
 def init():
-    pyg.init() #Simply initialises pyg and all its modules
-    pyg.font.init() #Initialise the fonts I want to use
-    display_list = pyg.display.get_desktop_sizes() #Obtains resolutions of all the displays
-    screen = pyg.display.set_mode(display_list[0],flags=pyg.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
-    pyg.display.set_caption("Title Screen") #Sets the title of the window to "Title Screen"
+    pygame.init() #Simply initialises pygame and all its modules
+    pygame.font.init() #Initialise the fonts I want to use
+    display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all the displays
+    screen = pygame.display.set_mode(display_list[0],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
+    pygame.display.set_caption("Title Screen") #Sets the title of the window to "Title Screen"
     
-    font = pyg.font.SysFont("Arial",40,True) #Added the titlefont
+    font = pygame.font.SysFont("Arial",40,True) #Added the titlefont
         
 
     # #A few more comments to explain the above:
     # #Essentially, the first part sets the resolution of the default display, the second part is a flag that ensures that it is fullscreen, the third enables the correct colours to be used, the fourth enables the default display to be selected, whilst the last one turns off Vsync
-    # main_menu = pyg_m.Menu("START",1920,1080, theme=pyg_m.themes.THEME_BLUE,) 
+    # main_menu = pygame_m.Menu("START",1920,1080, theme=pygame_m.themes.THEME_BLUE,) 
     # #main_menu.add_button("START","GAME STARTED")
-    # clock = pyg.time.Clock #Creates an object called clock to track time
+    # clock = pygame.time.Clock #Creates an object called clock to track time
 
-    # #pyg.set_caption("Menu") #Sets the title of the window to "Game"
+    # #pygame.set_caption("Menu") #Sets the title of the window to "Game"
