9b5f71a51d6708292e471fe8b3788dd0231f0b88
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-07 06:40:20 +0000

    Changed pyg to pygame in other files

--- classes..py
+++ classes..py
@@ -8,3 +8,4 @@ class Button:
         self.font = pygame.font.SysFont("Arial",40,True)
         self.rect = pygame.Rect(self.position,self.size)
         self.text_surface = self.font.render(self.text,True,(0,0,0))
+        
--- loop.py
+++ loop.py
@@ -1,9 +1,9 @@
-import pygame as pyg
+import pygame
 
 def game_loop(game_state):
     while game_state:
-        pyg.display.flip() #This updates the display
-        pyg.time.Clock().tick(250) #This is used to cap the frame rate to minimise unnecessary system resource usage
+        pygame.display.flip() #This updates the display
+        pygame.time.Clock().tick(250) #This is used to cap the frame rate to minimise unnecessary system resource usage
 
 def game_state_update():
     return 
