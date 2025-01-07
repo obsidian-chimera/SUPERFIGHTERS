d45a2dd6da117fb43f71267090f13c4479cb398e
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-07 07:04:07 +0000

    Started on the code to actually make the title screen and changed the loop to comment out trial code

--- loop.py
+++ loop.py
@@ -2,8 +2,13 @@ import pygame
 
 def game_loop(game_state):
     while game_state:
+        
+
+
+
         pygame.display.flip() #This updates the display
         pygame.time.Clock().tick(250) #This is used to cap the frame rate to minimise unnecessary system resource usage
+
 
 def game_state_update():
     return 
--- titleScreen.py
+++ titleScreen.py
@@ -1,2 +1,12 @@
 import pygame
 import sys
+from init import init
+from classes import Button
+
+init()
+
+start = Button("start","START GAME",(100,100),(200,200), (0,0,0))
+quit = Button("quit","QUIT GAME",(100,150),(200,200), (0,0,0))
+
+game_state = bool(True)
+while game_state:
