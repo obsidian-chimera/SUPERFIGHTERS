46d137bd93f29fbc53755513c76038b3e15e5e70
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-19 18:41:03 +0000

    Moved some of the extra comments in the main file to the comments file

--- titleScreen.py
+++ titleScreen.py
@@ -5,8 +5,7 @@ from classes import Button
 
 init()
 
-start = Button("start","START GAME",(100,100),(200,200), (0,0,0))
-quit = Button("quit","QUIT GAME",(100,150),(200,200), (0,0,0))
+
 
 game_state = bool(True)
 while game_state:
