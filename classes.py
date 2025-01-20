237d96b5af79d5aee7418d542897cc37048c3460
obsidian_chimera <rrajapakse2317@gmail.com>
2025-01-20 09:52:24 +0000

    I added quite a bit to this, where I made everything far more modular

--- classes.py
+++ classes.py
@@ -12,49 +12,19 @@ display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all th
 resolution = display_list[0]
 
 class Button:
-    def __init__(self, text, position, size, colour):
+    def __init__(self, text, position, size, colour, text_colour=BLACK):
         self.text = text
         self.position = position
         self.size = size
         self.colour = colour
         self.font = pygame.font.SysFont("Arial",40,True)
         self.rect = pygame.Rect(self.position,self.size)
-        self.surface = self.font.render(self.text,True,(0,0,0))
+        self.surface = self.font.render(self.text,True,text_colour)
 
         # Constructor method to setup attributes for the button
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.colour, self.rect)
-        self.surface.blit(self.text, (self.position[0] + self.size[0] // 2 - self.surface.get_width() // 2, self.position[1] + self.size[1] // 2 - self.surface.get_height() // 2))
+        screen.blit(self.surface, (self.position[0] + self.size[0] // 2 - self.surface.get_width() // 2, self.position[1] + self.size[1] // 2 - self.surface.get_height() // 2))
 
         # Method to draw the button onto the screen with the text superimposed on top of the button background
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(BLUE)
-        self.rect = self.image.get_rect()
-        self.rect.center = (resolution[0] // 2, resolution[1] // 2)
-
-    def update(self, keys):
-        if keys[pygame.K_LEFT] and self.rect.left > 0:
-            self.rect.x -= 5
-        if keys[pygame.K_RIGHT] and self.rect.right < resolution[0]:
-            self.rect.x += 5
-
-# Enemy sprite class
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (random.randint(0,resolution[0]), y)
-
-    def update(self):
-        self.rect.y += 3
-        if self.rect.top > resolution[1]:
-            self.rect.bottom = 0
-            self.rect.left = random.randint(0,resolution[0])
-    
-
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 pygame.init() #Simply initialises pygame and all its modules
 pygame.font.init() #Initialise the fonts I want to use
 from classes import Button
+from screens import main_menu
 
 
 WHITE = (255, 255, 255)
@@ -12,16 +13,32 @@ BLUE = (0, 0, 255)
 
 game_state = bool(False)
 
+def start_screen(screen_colour, title_colour, resolution):
+    screen.fill(screen_colour)
+    title_text = font.render("SUPERFIGHTERS",True , title_colour)
+    t_text_width = title_text.get_width()
+    t_text_height = title_text.get_height()
+
+    screen.blit(title_text, (resolution[0] // 2 - t_text_width // 2, resolution[1] // 2 - t_text_height // 2))
+    for button in screen_buttons:
+        button.draw(screen)
+
 
 display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all the displays
 resolution = display_list[0]
 screen = pygame.display.set_mode(display_list[0],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
 pygame.display.set_caption("Title Screen") #Sets the title of the window to "Title Screen"
 clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial",40,True) #Added the titlefont
+font = pygame.font.SysFont("Arial",260,True) #Added the titlefont
 timer_font = pygame.font.Font(None, 36)
 
-button = Button("START", (resolution[0] // 2 - 100, resolution[1] // 2 - 50), (200, 100), RED)
+screen_buttons = []
+screen_button = Button("START", ((resolution[0] - 100) // 2 -100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
+screen_exit_button = Button("EXIT", (((resolution[0] - 100) // 2) +100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
+screen_buttons.append(screen_button)
+screen_buttons.append(screen_exit_button)
+
+screen_selector = "start"
 
 
 if __name__ == "__main__":    
@@ -33,16 +50,24 @@ if __name__ == "__main__":
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     game_state = False
+            for start_button in screen_buttons:
+                if event.type == pygame.MOUSEBUTTONDOWN: #If the mouse is clicked
+                    if start_button.rect.collidepoint(event.pos): #If the mouse is clicked within the button boundaries 
+                        if start_button.text == "START": #Self explanatory 
+                            screen_selector = "MAIN"
+                            #Insert main game function here
+                        elif start_button.text == "EXIT":
+                            game_state = False
 
-
-        screen.fill(WHITE)
-        title_text = font.render("Title Screen",True , BLACK)
-        screen.blit(title_text, (resolution[0] // 2 - title_text.get_width() // 2, resolution[1] // 2 - title_text.get_height() // 2))
-        button.draw(screen)
+        if screen_selector == "start":
+            start_screen(BLACK, WHITE, resolution)
+        elif screen_selector == "MAIN":
+            start_screen(WHITE, BLACK, resolution)
+        
 
 
         pygame.display.flip()
-        clock.tick(250)
+        clock.tick(100)
 
     pygame.quit()
     sys.exit()
