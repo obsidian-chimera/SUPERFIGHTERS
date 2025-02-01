a3d1d1dac515e7ae2de6d5d382db287aefe55cd2
obsidian_chimera <rrajapakse2317@gmail.com>
2025-02-01 18:50:09 +0000

    I messed up the button layout completely

--- classes.py
+++ classes.py
@@ -18,13 +18,20 @@ class Button:
         self.size = size
         self.colour = colour
         self.font = pygame.font.SysFont("Arial",40,True)
-        self.rect = pygame.Rect(self.position,self.size)
         self.surface = self.font.render(self.text,True,text_colour)
+        text_width, text_height = self.surface.get_size()
+        new_width = max(self.size[0], text_width + 20)
+        new_height = max(self.size[1], text_height + 20)
+        self.rect = pygame.Rect(self.position, (new_width, new_height))
 
         # Constructor method to setup attributes for the button
 
+
     def draw(self, screen):
+        text_width, text_height = self.surface.get_size()
+        new_width = max(self.size[0], text_width + 20)
+        new_height = max(self.size[1], text_height + 20)
         pygame.draw.rect(screen, self.colour, self.rect)
-        screen.blit(self.surface, (self.position[0] + self.size[0] // 2 - self.surface.get_width() // 2, self.position[1] + self.size[1] // 2 - self.surface.get_height() // 2))
+        screen.blit(self.surface, (new_width + self.size[0] // 2 - new_width // 2, new_height + self.size[1] // 2 - new_height // 2))
 
         # Method to draw the button onto the screen with the text superimposed on top of the button background
