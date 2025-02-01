c58366b3106ac2a44e0db447ab30c4a60f390d1f
obsidian_chimera <rrajapakse2317@gmail.com>
2025-02-01 15:47:33 +0000

    Updated and removed redundant files into an older files folder to streamline the development process

--- screens.py
+++
@@ -1,5 +0,0 @@
-def main_menu():        
-    screen.fill(BLACK)
-    title_text = font.render("SUPERFIGHTERS",True , WHITE)
-    screen.blit(title_text, (resolution[0] // 2 - title_text.get_width() // 2, resolution[1] // 2 - title_text.get_height() // 2))
-    button.draw(screen)
