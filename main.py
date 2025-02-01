f4c28381a5e3cf32f7dcb84c4d434cfb869f7972
obsidian_chimera <rrajapakse2317@gmail.com>
2025-02-01 18:33:06 +0000

    I ran into an interersting issues where artifacts still remain so if I click in the same position on the choice screen where my old rect for exiting the game used to be, it exits the game but there shouldn't be an option for it

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ BLUE = (127, 255, 212)
 
 game_state = bool(False)
 
+display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all the displays
+resolution = display_list[0]
+screen = pygame.display.set_mode(display_list[0],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
+pygame.display.set_caption("Title Screen") #Sets the title of the window to "Title Screen"
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial",260,True) #Added the titlefont
+timer_font = pygame.font.Font(None, 36)
+
+
+screen_buttons = []
+screen_button = Button("START", ((resolution[0] - 100) // 2 -100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
+screen_exit_button = Button("EXIT", (((resolution[0] - 100) // 2) +100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
+screen_buttons.append(screen_button)
+screen_buttons.append(screen_exit_button)
+
 def start_screen(screen_colour, title_colour, resolution):
     screen.fill(screen_colour)
     title_text = font.render("SUPERFIGHTERS",True , title_colour)
@@ -25,30 +40,24 @@ def start_screen(screen_colour, title_colour, resolution):
         button.draw(screen)
 
 
-display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all the displays
-resolution = display_list[0]
-screen = pygame.display.set_mode(display_list[0],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
-pygame.display.set_caption("Title Screen") #Sets the title of the window to "Title Screen"
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial",260,True) #Added the titlefont
-timer_font = pygame.font.Font(None, 36)
-
-screen_buttons = []
-screen_button = Button("START", ((resolution[0] - 100) // 2 -100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
-screen_exit_button = Button("EXIT", (((resolution[0] - 100) // 2) +100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
-screen_buttons.append(screen_button)
-screen_buttons.append(screen_exit_button)
-
 choice_buttons = []
-one_player = Button("ONE PLAYER", ((resolution[0] - 100) // 2 -100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
-two_player = Button("TWO PLAYER", (((resolution[0] - 100) // 2) +100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
+one_player = Button("ONE PLAYER", ((resolution[0] - 100) // 4, 3 * ((resolution[1] - 100) // 4)), (100, 100), WHITE)
+two_player = Button("TWO PLAYER", (((resolution[0] - 100) // 4), 3 * ((resolution[1] - 250) // 4)), (100, 100), WHITE)
 choice_buttons.append(one_player)
 choice_buttons.append(two_player)
 
+def choice_screen(screen_colour, title_colour, resolution):
+    screen.fill(screen_colour)
+    title_text = font.render("SUPERFIGHTERS",True , title_colour)
+    main_text_width = title_text.get_width()
+    main_text_height = title_text.get_height()
+
+    screen.blit(title_text, (resolution[0] // 2 - main_text_width // 2, resolution[1] // 2 - main_text_height // 2))
+    for button in choice_buttons:
+        button.draw(screen)
 
 
 screen_selector = "start"
-
 
 if __name__ == "__main__":    
     game_state = True
@@ -59,32 +68,38 @@ if __name__ == "__main__":
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     game_state = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_loc = pygame.mouse.get_pos()
+
+
             for start_button in screen_buttons:
                 if event.type == pygame.MOUSEBUTTONDOWN: #If the mouse is clicked
-                    if start_button.rect.collidepoint(event.pos): #If the mouse is clicked within the button boundaries 
+                    if start_button.rect.collidepoint(mouse_loc): #If the mouse is clicked within the button boundaries 
                         if start_button.text == "START": #Self explanatory 
                             screen_selector = "MAIN"
-                            #Insert main game function here
                         elif start_button.text == "EXIT":
                             game_state = False
+            for choice_button in choice_buttons:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if choice_button.rect.collidepoint(mouse_loc):
+                        if choice_button.text == "ONE PLAYER":
+                            screen_selector = "ONE PLAYER"
+                            print("ONE PLAYER")
+                        elif choice_button.text == "TWO PLAYER":
+                            screen_selector = "TWO PLAYER"
+                            print("TWO PLAYER")
 
         if screen_selector == "start":
             start_screen(BLACK, WHITE, resolution)
 
-        elif screen_selector == "MAIN":
-                screen.fill(BLUE)
-                title_text = font.render("SUPERFIGHTERS",True , BLACK)
-                main_text_width = title_text.get_width()
-                main_text_height = title_text.get_height()
+        if screen_selector == "MAIN":
+            choice_screen(BLUE, WHITE, resolution)
 
-                screen.blit(title_text, (resolution[0] // 2 - main_text_width // 2, resolution[1] // 2 - main_text_height // 2))
-                for button in choice_buttons:
-                    button.draw(screen)
         
 
 
         pygame.display.flip()
-        clock.tick(100)
+        clock.tick(60)
 
     pygame.quit()
     sys.exit()
