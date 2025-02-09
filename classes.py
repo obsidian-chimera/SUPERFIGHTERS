6a82cb192e7f59c5aa1fd2c5b8be9f9ff812454b
obsidian_chimera <rrajapakse2317@gmail.com>
2025-02-09 16:23:50 +0000

    added loads of game mechanics stuff based off of the YT video tutorial

--- classes.py
+++ classes.py
@@ -19,9 +19,9 @@ class Button:
         self.colour = colour
         self.font = pygame.font.SysFont("Arial",40,True)
         self.surface = self.font.render(self.text,True,text_colour)
-        text_width, text_height = self.surface.get_size()
-        self.width = text_width + 20
-        self.height = text_height + 20
+        self.text_width, self.text_height = self.surface.get_size()
+        self.width = self.text_width + 20
+        self.height = self.text_height + 20
         self.rect = pygame.Rect(self.position, (self.width, self.height))
 
         # Constructor method to setup attributes for the button
@@ -48,4 +48,60 @@ class box:
 
     def draw(self, screen):
         screen.blit(self.surface, self.rect)
+    
+class object(pygame.sprite.Sprite):
+    def __init__(self, position, image, classification):
+        super().__init__(classification)
+        self.image = image
+        self.rect = self.image.get_frect(topleft = position)
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+class Player(object):
+    def __init__(self, position, image, classification):
+        super().__init__(position, image, classification)
+        self.health = 100
+        self.speed = 5
+        self.jump = 10
+        self.gravity = 1
+        self.bullets = []
+        self.weapon = None
+        self.direction = 1
+        self.jumping = False
+        self.falling = False
+        self.shooting = False
+        self.dead = False
+        self.score = 0
+        self.kills = 0
+        self.deaths = 0
+        self.damage = 10
+        self.direction = 1
+    
+    def move(self, direction):
+        self.rect.x += direction * self.speed
+
+    def jump(self):
+        self.rect.y -= self.jump
+        self.jumping = True
+    
+    def key_down(self, key):
+        if key == pygame.K_w:
+            self.jump()
+        if key == pygame.K_a:
+            self.move(-1)
+        if key == pygame.K_d:
+            self.move(1)
+        if key == pygame.K_SPACE:
+            self.jump()
+
+class bot:
+    pass
+
+class bullet:
+    pass
+
+class weapon:
+    pass
 
--- main.py
+++ main.py
@@ -1,195 +1,192 @@
 import pygame
 import sys
-import pytmx #Imported the pytmx module
-pygame.init() #Simply initialises pygame and all its modules
-pygame.font.init() #Initialise the fonts I want to use
-from classes import Button, box
-
-
+pygame.init()  # Simply initialises pygame and all its modules
+pygame.font.init()  # Initialise the fonts I want to use
+from classes import *  # Imported the classes from the classes.py file
+import pytmx
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (127, 255, 212)
 
-game_state = bool(False)
-
-pygame.display.set_caption("Title Screen") #Sets the title of the window to "Title Screen"
-icon = pygame.image.load("./images/icon.png") #Loads the image "icon.png" into the variable "icon"
+icon = pygame.image.load("./images/icon.png")  # Loads the image "icon.png" into the variable "icon"
 icon = pygame.transform.scale(icon, (500, 500))
-pygame.display.set_icon(icon) #Sets the icon of the window to the image "icon.png"
-display_list = pygame.display.get_desktop_sizes() #Obtains resolutions of all the displays
+display_list = pygame.display.get_desktop_sizes()  # Obtains resolutions of all the displays
 resolution = display_list[0]
-screen = pygame.display.set_mode(display_list[0],flags=pygame.FULLSCREEN,depth=0,display=0,vsync=0) #A bit more complex here in that it renders the display to my monitor's resolution, 
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial",260,True) #Added the titlefont
+font = pygame.font.SysFont("Arial", 260, True)  # Added the titlefont
 small_title = pygame.font.SysFont("Arial", 100, True)
 timer_font = pygame.font.Font(None, 36)
 
+game_state = False
 
-def load_tmx_map(filename):
-    return pytmx.load_pygame(filename)
+class Game:
+    def __init__(self):
+        pygame.display.set_caption("Title Screen")  # Sets the title of the window to "Title Screen"
+        pygame.display.set_icon(icon)  # Sets the icon of the window to the image "icon.png"
+        self.screen = pygame.display.set_mode(display_list[0], flags=pygame.FULLSCREEN, depth=0, display=0, vsync=0)
+        self.clock = pygame.time.Clock()
+        self.map = None
+        self.buttons = []
+        self.boxes = []
+        self.sprites = pygame.sprite.Group()
+        self.collision = pygame.sprite.Group()
 
-map_one = load_tmx_map("./Maps/base_map.tmx")
+    def load_tmx_map(self, filename):
+        return pytmx.load_pygame(filename)
 
-def render_map(surface, tmx_data):
-    for layer in tmx_data.visible_layers:
-        if isinstance(layer, pytmx.TiledTileLayer):
-            for x, y, gid in layer:
-                tile = tmx_data.get_tile_image_by_gid(gid)
-                if tile:
-                    surface.blit(tile, (x * tmx_data.tilewidth, y * tmx_data.tileheight))
+    def render_map(self, surface, tmx_data):
+        for layer in tmx_data.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid in layer:
+                    tile = tmx_data.get_tile_image_by_gid(gid)
+                    if tile:
+                        surface.blit(tile, (x * tmx_data.tilewidth, y * tmx_data.tileheight))
 
+    def start_screen(self, screen_colour, title_colour, resolution):
+        self.buttons = []
+        self.boxes = []
+        screen_button = Button("START", ((resolution[0] - 100) // 2 - 100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
+        screen_exit_button = Button("EXIT", (((resolution[0] - 100) // 2) + 100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
+        self.buttons += [screen_button, screen_exit_button]
 
-screen_buttons = []
-screen_button = Button("START", ((resolution[0] - 100) // 2 -100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
-screen_exit_button = Button("EXIT", (((resolution[0] - 100) // 2) +100, 3 * ((resolution[1] - 100) // 4)), (100, 100), BLACK, WHITE)
-screen_buttons.append(screen_button)
-screen_buttons.append(screen_exit_button)
+        self.screen.fill(screen_colour)
+        title_text = font.render("SUPERFIGHTERS", True, title_colour)
+        t_text_width = title_text.get_width()
+        t_text_height = title_text.get_height()
 
-def start_screen(screen_colour, title_colour, resolution):
-    screen.fill(screen_colour)
-    title_text = font.render("SUPERFIGHTERS",True , title_colour)
-    t_text_width = title_text.get_width()
-    t_text_height = title_text.get_height()
+        self.screen.blit(title_text, (resolution[0] // 2 - t_text_width // 2, resolution[1] // 2 - t_text_height // 2))
+        for button in self.buttons:
+            button.draw(self.screen)
 
-    screen.blit(title_text, (resolution[0] // 2 - t_text_width // 2, resolution[1] // 2 - t_text_height // 2))
-    for button in screen_buttons:
-        button.draw(screen)
+    def choice_screen(self, screen_colour, title_colour, resolution):
+        self.buttons = []
+        self.boxes = []
+        one_player = Button("ONE PLAYER", ((resolution[0] - 100) // 7, (resolution[1] // 2)), (100, 100), WHITE)
+        two_player = Button("TWO PLAYER", ((resolution[0] - 100) // 7, (resolution[1] // 2) + 100), (100, 100), WHITE)
+        tutorial = Button("TUTORIAL", ((resolution[0] - 100) // 7, (resolution[1] // 2) + 200), (100, 100), WHITE)
+        setup = Button("SETUP", ((resolution[0] - 100) // 7, (resolution[1] // 2) + 300), (100, 100), WHITE)
+        self.buttons += [one_player, two_player, tutorial, setup]
 
+        pygame.display.set_caption("Game Modes")
+        self.screen.fill(screen_colour)
+        title_text = font.render("SUPERFIGHTERS", True, title_colour)
+        main_text_width = title_text.get_width()
+        main_text_height = title_text.get_height()
 
-choice_buttons = []
-one_player = Button("ONE PLAYER", ((resolution[0] - 100) // 7, ((resolution[1]) // 2)), (100, 100), WHITE)
-two_player = Button("TWO PLAYER", ((resolution[0] - 100) // 7, ((resolution[1]) // 2)+100), (100, 100), WHITE)
-tutorial = Button("TUTORIAL", ((resolution[0] - 100) // 7, ((resolution[1]) // 2)+200), (100, 100), WHITE)
-setup = Button("SETUP", ((resolution[0] - 100) // 7, ((resolution[1]) // 2)+300), (100, 100), WHITE)
-choice_buttons.append(one_player)
-choice_buttons.append(two_player)
-choice_buttons.append(tutorial)
-choice_buttons.append(setup)
+        self.screen.blit(title_text, (resolution[0] // 2 - main_text_width // 2, (resolution[1] - 400) // 2 - main_text_height // 2))
+        for button in self.buttons:
+            button.draw(self.screen)
 
-def choice_screen(screen_colour, title_colour, resolution):
-    pygame.display.set_caption("Game Modes")
-    screen.fill(screen_colour)
-    title_text = font.render("SUPERFIGHTERS",True , title_colour)
-    main_text_width = title_text.get_width()
-    main_text_height = title_text.get_height()
+    def oneplayer_screen(self, screen_colour, title_colour, resolution):
+        self.buttons = []
+        self.boxes = []
+        box_width = 500
+        box_height = 500
+        onep_box = box((box_width, box_height), ((resolution[0] - box_width) // 2, (resolution[1] - box_height) // 2), WHITE)
+        onep_box.surface.blit(icon, (0, 0))
+        self.boxes.append(onep_box)
 
-    screen.blit(title_text, ((resolution[0]) // 2 - main_text_width // 2, (resolution[1]-400) // 2 - main_text_height // 2))
-    for button in choice_buttons:
-        button.draw(screen)
+        start_game = Button("BEGIN", ((((resolution[0] - 100) // 2), 3 * ((resolution[1]- 100) // 4)+60)), (100, 100), WHITE)
+        self.buttons.append(start_game)
 
-# onep_buttons = []
-# one_player = Button("ONE PLAYER", ((resolution[0] - 100) // 7, ((resolution[1]) // 2)), (100, 100), WHITE)
-# two_player = Button("TWO PLAYER", ((resolution[0] - 100) // 7, ((resolution[1]) // 2)+100), (100, 100), WHITE)
-# tutorial = Button("TUTORIAL", ((resolution[0] - 100) // 7, ((resolution[1]) // 2)+200), (100, 100), WHITE)
-# setup = Button("SETUP", ((resolution[0] - 100) // 7, ((resolution[1]) // 2)+300), (100, 100), WHITE)
-# onep_buttons.append(one_player)
-# onep_buttons.append(two_player)
-# onep_buttons.append(tutorial)
-# onep_buttons.append(setup)
+        pygame.display.set_caption("One Player Mode")
+        self.screen.fill(screen_colour)
+        
+        title_text = small_title.render("SUPERFIGHTERS", True, title_colour)
+        main_text_width = title_text.get_width()
+        main_text_height = title_text.get_height()
+        self.screen.blit(title_text, ((resolution[0] - main_text_width) // 2, (resolution[1] - 800 - main_text_height) // 2))
+        for box_item in self.boxes:
+            box_item.draw(self.screen)
+        for button in self.buttons:
+            button.draw(self.screen)
 
-onep_boxes = []
-box_width = 500
-box_height = 500
-onep_box = box((box_width,box_height), ((resolution[0] - box_width) // 2, ((resolution[1] - box_height) // 2)), WHITE)
-onep_box.surface.blit(icon)
-onep_boxes.append(onep_box)
+    def gameplay_screen(self):
+        dt = self.clock.tick(60) / 1000 
+        self.buttons = []
+        self.boxes = []
+        pygame.display.set_caption("Gameplay")
+        self.map = self.load_tmx_map("./Maps/world.tmx")
+        self.screen.fill(WHITE)
+        self.level_width = self.map.width * 64
+        self.level_height = self.map.height * 64
 
-onep_buttons = []
-start_game = Button("BEGIN",((resolution[0] // 2) -120, 3 * ((resolution[1] // 4))), (100, 100), WHITE)
-onep_buttons.append(start_game)
+        for x, y, image in self.map.get_layer_by_name('Main').tiles():
+            object((x * 64,y * 64), image, (self.sprites, self.collision))
+        
+        for x, y, image in self.map.get_layer_by_name('Decoration').tiles():
+            object((x * 64,y * 64), image, (self.sprites))
 
-def oneplayer_screen(screen_colour, title_colour, resolution):
-    pygame.display.set_caption("One PLayer Mode")
-    screen.fill(screen_colour)
-    title_text = small_title.render("SUPERFIGHTERS",True , title_colour)
-    main_text_width = title_text.get_width()
-    main_text_height = title_text.get_height()
-    screen.blit(title_text, ((resolution[0]- main_text_width) // 2, (resolution[1]-800 - main_text_height) // 2))
-    for box in onep_boxes:
-        box.draw(screen)
-    for button in onep_buttons:
-        button.draw(screen)
-
-
-def gameplay_screen():
-    pygame.display.set_caption("Gameplay")
-    render_map(screen, map_one)
-
-
-
-
-
-
-
-
+        
+        for obj in self.map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                player_img = pygame.image.load("./images/player.webp").convert_alpha()
+                player_img = pygame.transform.scale(player_img, (50, 50))
+                Player((obj.x, obj.y), player_img, (self.sprites, self.collision))
 
 
+        self.sprites.draw(self.screen)
 
-
-screen_selector = "start"
-
-if __name__ == "__main__":    
-    game_state = True
-    while game_state:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_state = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+    def run(self):
+        screen_selector = "start"
+        game_state = True
+        while game_state:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     game_state = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_loc = pygame.mouse.get_pos()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game_state = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_loc = pygame.mouse.get_pos()
 
+                if screen_selector == "start":
+                    for start_button in self.buttons:
+                        if event.type == pygame.MOUSEBUTTONDOWN:  # If the mouse is clicked
+                            if start_button.rect.collidepoint(mouse_loc):  # If the mouse is clicked within the button boundaries 
+                                if start_button.text == "START":  # Self explanatory 
+                                    screen_selector = "MAIN"
+                                elif start_button.text == "EXIT":
+                                    game_state = False
+
+                if screen_selector == "MAIN":
+                    for choice_button in self.buttons:
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            if choice_button.rect.collidepoint(mouse_loc):
+                                if choice_button.text == "ONE PLAYER":
+                                    screen_selector = "ONE PLAYER"
+                                    print("ONE PLAYER")
+                                elif choice_button.text == "TWO PLAYER":
+                                    screen_selector = "TWO PLAYER"
+                                    print("TWO PLAYER")
+
+                if screen_selector == "ONE PLAYER":
+                    for onep_button in self.buttons:
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            if onep_button.rect.collidepoint(mouse_loc):
+                                if onep_button.text == "BEGIN":
+                                    screen_selector = "GAMEPLAY"
+                                    print("GAMEPLAY")
+                    
             if screen_selector == "start":
-                for start_button in screen_buttons:
-                    if event.type == pygame.MOUSEBUTTONDOWN: #If the mouse is clicked
-                        if start_button.rect.collidepoint(mouse_loc): #If the mouse is clicked within the button boundaries 
-                            if start_button.text == "START": #Self explanatory 
-                                screen_selector = "MAIN"
-                            elif start_button.text == "EXIT":
-                                game_state = False
+                self.start_screen(BLACK, WHITE, resolution)
 
             if screen_selector == "MAIN":
-                for choice_button in choice_buttons:
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if choice_button.rect.collidepoint(mouse_loc):
-                            if choice_button.text == "ONE PLAYER":
-                                screen_selector = "ONE PLAYER"
-                                print("ONE PLAYER")
-                            elif choice_button.text == "TWO PLAYER":
-                                screen_selector = "TWO PLAYER"
-                                print("TWO PLAYER")
-
+                self.choice_screen(BLACK, WHITE, resolution)
+            
             if screen_selector == "ONE PLAYER":
-                for onep_button in onep_buttons:
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if onep_button.rect.collidepoint(mouse_loc):
-                            if onep_button.text == "BEGIN":
-                                screen_selector = "GAMEPLAY"
-                                print("GAMEPLAY")
-                
+                self.oneplayer_screen(BLACK, WHITE, resolution)
 
-        if screen_selector == "start":
-            start_screen(BLACK, WHITE, resolution)
+            if screen_selector == "GAMEPLAY":
+                self.gameplay_screen()
 
-        if screen_selector == "MAIN":
-            choice_screen(BLACK, WHITE, resolution)
-        
-        if screen_selector == "ONE PLAYER":
-            oneplayer_screen(BLACK, WHITE, resolution)
+            pygame.display.update()
+            self.clock.tick(60)
 
-        if screen_selector == "GAMEPLAY":
-            gameplay_screen()
+        pygame.quit()
+        sys.exit()
 
-        
-
-
-        pygame.display.update()
-        clock.tick(60)
-
-    pygame.quit()
-    sys.exit()
-
-
+if __name__ == "__main__":
+    superfighters = Game()
+    superfighters.run()
