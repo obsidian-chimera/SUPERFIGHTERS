afd03ec79f2ba45071840c9b61304619d93b16c0
obsidian_chimera <rrajapakse2317@gmail.com>
2025-02-11 08:29:15 +0000

    Uncommented out a few wrong lines

--- classes.py
+++ classes.py
@@ -90,39 +90,39 @@ class Player(object):
         if keys[pygame.K_SPACE] and self.ground:
              self.direction.y = -1
     
-    # def collisions(self, direction):
-    #     for sprite in self.collision:
-    #         if sprite.rect.colliderect(self.rect):
-    #             if direction == 'horizontal':
-    #                 if self.direction.x > 0: 
-    #                     self.rect.right = sprite.rect.left
-    #                 if self.direction.x < 0: 
-    #                     self.rect.left = sprite.rect.right
-    #                 self.direction.x = 0
-    #             if direction == 'vertical':
-    #                 if self.direction.y > 0: 
-    #                     self.rect.bottom = sprite.rect.top
-    #                 if self.direction.y < 0: 
-    #                     self.rect.top = sprite.rect.bottom
-    #                 self.direction.y = 0
+    def collisions(self, direction):
+        for sprite in self.collision:
+            if sprite.rect.colliderect(self.rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: 
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0: 
+                        self.rect.left = sprite.rect.right
+                    self.direction.x = 0
+                if direction == 'vertical':
+                    if self.direction.y > 0: 
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: 
+                        self.rect.top = sprite.rect.bottom
+                    self.direction.y = 0
 
     def move(self, dt):
         # Horizontal movement
         prev_x = self.rect.x
         self.rect.x += self.direction.x * self.speed * dt
-        # self.collisions('horizontal')
+        self.collisions('horizontal')
 
         # Vertical movement
         prev_y = self.rect.y
         self.rect.y += self.direction.y * self.gravity * dt
 
-        # self.collisions('vertical')
+        self.collisions('vertical')
 
 
 
-    # def check_floor(self):
-    #     bottom_rect = pygame.FRect((0,0), (self.rect.width, 2)).move_to(midtop = self.rect.midbottom)
-    #     self.ground = True if bottom_rect.collidelist([sprite.rect for sprite in self.collision]) >= 0 else False
+    def check_floor(self):
+        bottom_rect = pygame.FRect((0,0), (self.rect.width, 2)).move_to(midtop = self.rect.midbottom)
+        self.ground = True if bottom_rect.collidelist([sprite.rect for sprite in self.collision]) >= 0 else False
     
     # def move(self, direction):
     #     self.rect.x += direction * self.speed
@@ -142,7 +142,7 @@ class Player(object):
     #         self.jump()
     
     def update(self, dt):
-        # self.check_floor()
+        self.check_floor()
         self.input()
         self.move(dt)
 
